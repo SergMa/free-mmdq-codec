418fb2631b54fe3db484ec8c0e7c3e50734b90b2
Serg Mashkin <mashkh@yandex.ru>
2015-07-10 19:04:23 +0600

    MATLAB: some experiments with compand, expand functions
    piecewise linear function

--- matlab/compand.m
+++ matlab/compand.m
@@ -6,38 +6,66 @@ function y = compand( x , ver )
     case 1
         % VER1: power 1.3 table
         a = -1:0.01:1;
-        table = sign(a) .* abs(a) .^ (1/1.3);
+        table = sign(a) .* abs(a) .^ (1/1.2);
 
     case 2
         % VER2: power 1.5 table
-        a = -1:0.01:1;
-        table = sign(a) .* abs(a) .^ (1/1.5);
+        %a = -1:0.01:1;
+        %table = sign(a) .* abs(a) .^ (1/1.4);
+
+        a = 0.01:0.01:1;
+        N=length(a);
+        table1 = zeros(1,N);
+        for i=1:N
+            if a(i)<=0.3
+                table1(i) = (0.1/0.3)*a(i);
+            elseif a(i)<=0.7
+                table1(i) = (0.8/0.4)*(a(i)-0.3) + 0.1;
+            else
+                table1(i) = (0.1/0.3)*(a(i)-0.7) + 0.9;
+            end
+        end
+        table = [ -fliplr(table1) , 0 , table1 ];
 
     case 3
-        % VER3: power 1.8 table
-        a = -1:0.01:1;
-        table = sign(a) .* abs(a) .^ (1/1.8);
+        % VER3: power 0.7 table
+        %a = -1:0.01:1;
+        %table = sign(a) .* abs(a) .^ (1/1.8);
+
+        a = 0.01:0.01:1;
+        N=length(a);
+        table1 = zeros(1,N);
+        for i=1:N
+            if a(i)<=0.1
+                table1(i) = (0.3/0.1)*a(i);
+            elseif a(i)<=0.9
+                table1(i) = (0.4/0.8)*(a(i)-0.1) + 0.3;
+            else
+                table1(i) = (0.3/0.1)*(a(i)-0.9) + 0.7;
+            end
+        end
+        table = [ -fliplr(table1) , 0 , table1 ];
 
     case 4
-        % VER2: power 2 table
+        % VER4: power 2 table
         a = -1:0.01:1;
         table = sign(a) .* abs(a) .^ (1/2);
 
     case 5
-        % VER3: power 1.2 table
+        % VER5: power 1.2 table
         a = -1:0.01:1;
         table = sign(a) .* abs(a) .^ (1/1.2);
 
     case 6
-        % VER4: A-compand
+        % VER6: A-compand
         a = -1:0.01:1;
         table = a_compand(a);
 
     case 7
-        % VER6
-        % manually created conversion table
-        %          0.05  0.10  0.15  0.20  0.25  0.30  0.35  0.40  0.45  0.50  0.55  0.60  0.65  0.70  0.75  0.80  0.85  0.90  0.95  1.00   % linear
-        table1 = [ 0.01  0.02  0.03  0.05  0.10  0.15  0.20  0.25  0.30  0.35  0.40  0.45  0.50  0.55  0.60  0.65  0.70  0.80  0.90  1.00]; % output
+        % VER7
+        % manually created conversion table (LINEAR-TO-LINEAR)
+        %          0.05  0.10  0.15  0.20  0.25  0.30  0.35  0.40  0.45  0.50  0.55  0.60  0.65  0.70  0.75  0.80  0.85  0.90  0.95  1.00    % linear
+        table1 = [ 0.05  0.10  0.15  0.20  0.25  0.30  0.35  0.40  0.45  0.50  0.55  0.60  0.65  0.70  0.75  0.80  0.85  0.90  0.95  1.00 ]; % output
         table = [ -fliplr(table1) , 0 , table1 ];
     end
 
