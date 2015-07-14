391a8c4b77f7ca8084d6588dfc464033add20a69
Serg Mashkin <mashkh@yandex.ru>
2015-07-14 14:07:24 +0600

    C: demo.sh for demonstration of codecs, MSE estimations

---
+++ c/demo.sh
@@ -0,0 +1,55 @@
+#!/bin/sh 
+
+################################################################################
+# Demonstration of test programm                                               #
+# demo.sh                                                                      #
+# (c) Sergei Mashkin, 2015                                                     #
+################################################################################
+
+echo "Demonstration of test program"
+
+echo "Encode sample sound with MMDQ-codec:"
+
+out/test --mmdq-encode 14 3 1 out/input.wav out/mmdq-32kbps.bin
+out/test --mmdq-encode  7 3 1 out/input.wav out/mmdq-40kbps.bin
+
+echo "Encode sample sound with G726-codec:"
+
+out/test --g726-encode 32 out/input.wav out/g726-32kbps.bin
+out/test --g726-encode 40 out/input.wav out/g726-40kbps.bin
+
+echo "Encode sample sound with G711-codec:"
+out/test --g711-encode alaw out/input.wav out/g711-alaw.bin
+out/test --g711-encode ulaw out/input.wav out/g711-ulaw.bin
+
+
+echo "Decode sample sound with MMDQ-codec:"
+
+out/test --mmdq-decode 14 3 1 out/mmdq-32kbps.bin out/mmdq-32kbps.wav
+out/test --mmdq-decode  7 3 1 out/mmdq-40kbps.bin out/mmdq-40kbps.wav
+ 
+echo "Decode sample sound with G726-codec:"
+
+out/test --g726-decode 32 out/g726-32kbps.bin out/g726-32kbps.wav
+out/test --g726-decode 40 out/g726-40kbps.bin out/g726-40kbps.wav
+
+echo "Decode sample sound with G711-codec:"
+out/test --g711-encode alaw out/g711-alaw.bin out/g711-alaw.wav 
+out/test --g711-encode ulaw out/g711-ulaw.bin out/g711-ulaw.wav 
+
+
+echo "Measure quality of MMDQ-codec:"
+
+out/test --mse 0 out/input.wav out/mmdq-32kbps.wav
+out/test --mse 0 out/input.wav out/mmdq-40kbps.wav
+
+echo "Measure quality of G726-codec:"
+
+out/test --mse 0 out/input.wav out/g726-32kbps.wav
+out/test --mse 0 out/input.wav out/g726-40kbps.wav
+
+echo "Measure quality of G711-codec:"
+
+out/test --mse 0 out/input.wav out/g711-alaw.wav
+out/test --mse 0 out/input.wav out/g711-ulaw.wav
+
