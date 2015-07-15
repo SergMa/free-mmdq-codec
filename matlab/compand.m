28ec682712a0b123f68b76d099b8ab89ff035b7a
Serg Mashkin <mashkh@yandex.ru>
2015-07-15 17:07:10 +0600

    MATLAB: remove tables from compand(), expand() - works more faster now

--- matlab/compand.m
+++ matlab/compand.m
@@ -6,46 +6,27 @@ function y = compand( x , ver )
     switch ver
     case 0
         % VER0: linear table
-        a = -1:0.001:1;
-        table = a;
+        y = x;
 
     case 1
         %% VER1: power 1.2 table
-        %a = -1:0.001:1;
-        %table = sign(a) .* abs(a) .^ (1/1.2);
+        %y = sign(x) .* abs(x) .^ (1/1.2);
 
         % VER1: power 1.5 table
-        a = -1:0.001:1;
-        table = sign(a) .* abs(a) .^ (1.5);
+        y = sign(x) .* abs(x) .^ (1/1.5);
 
     case 2
         %% VER2: power 1.4 table
-        %a = -1:0.001:1;
-        %table = sign(a) .* abs(a) .^ (1/1.4);
+        %y = sign(x) .* abs(x) .^ (1/1.4);
 
         % VER2: power 0.65 table
-        a = -1:0.001:1;
-        table = sign(a) .* (1 - (1 - abs(a)) .^ (1/0.65));
+        y = sign(x) .* (1 - (1 - abs(x)) .^ (1/0.65));
 
     case 3
         %% VER2: power 1.8 table
-        %a = -1:0.001:1;
-        %table = sign(a) .* abs(a) .^ (1/1.8);
+        %y = sign(x) .* abs(x) .^ (1/1.8);
 
         % VER3: power 1.5 table
-        a = -1:0.001:1;
-        table = sign(a) .* (1 - (1 - abs(a)) .^ (1/1.5));
+        y = sign(x) .* (1 - (1 - abs(x)) .^ (1/1.5));
     end
-
-    %convert x into [1,2,...,N] range
-    N = length(table);
-    p = round( (x+1.0)*(N-1)/2 + 1 );
-    if p<1
-        p = 1;
-    elseif p>N
-        p = N;
-    end
-
-    y = table(p);
-
 return
