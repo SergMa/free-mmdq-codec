72a583d73a50a9e2bb7350985b2b31238a2b8a51
Serg Mashkin <mashkh@yandex.ru>
2015-07-15 17:08:03 +0600

    MATLAB: main_compand_expand - add expand(compand(x)) plots

--- matlab/main_compand_expand.m
+++ matlab/main_compand_expand.m
@@ -18,9 +18,14 @@ e1 = expand(x,1);
 e2 = expand(x,2);
 e3 = expand(x,3);
 
+z0 = expand(compand(x,0),0);
+z1 = expand(compand(x,0),0);
+z2 = expand(compand(x,0),0);
+z3 = expand(compand(x,0),0);
+
 figure(1);
 plot(x,c0,'r.-', x,c1,'g.-', x,c2,'b.-', x,c3,'k.-');
-title('compand');
+title('compand(x)');
 legend('ver0','ver1','ver2','ver3');
 xlabel('x');
 ylabel('y');
@@ -28,7 +33,15 @@ grid on;
 
 figure(2);
 plot(x,e0,'r.-', x,e1,'g.-', x,e2,'b.-', x,e3,'k.-');
-title('expand');
+title('expand(x)');
+legend('ver0','ver1','ver2','ver3');
+xlabel('x');
+ylabel('y');
+grid on;
+
+figure(2);
+plot(x,z0,'r.-', x,z1,'g.-', x,z2,'b.-', x,z3,'k.-');
+title('expand(compand(x))');
 legend('ver0','ver1','ver2','ver3');
 xlabel('x');
 ylabel('y');
