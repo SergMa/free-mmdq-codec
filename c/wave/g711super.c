3948244f843223109ecb152b053602504bc315c5
Serg Mashkin <mashkh@yandex.ru>
2015-07-21 14:58:02 +0600

    C: kill bug of g711 codec (use SpanDSP code)

--- c/wave/g711super.c
+++ c/wave/g711super.c
@@ -4,9 +4,22 @@
 /* (c) TODO: find authors                                                     */
 /******************************************************************************/
 
+/* Compilation of DAHDI, SpanDSP codes */
+
+/*
+ * SpanDSP - a series of DSP components for telephony
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1,
+ * as published by the Free Software Foundation.
+ *
+ * Written by Steve Underwood <steveu@coppice.org>
+ * Copyright (C) 2001 Steve Underwood
+ */
+
 #include "g711super.h"
 
-//======== DECOMPRESS FUNCTIONS ===============================
+//======== DECOMPRESS FUNCTIONS ================================================
 
 short MuLawDecompressTable[256] =
 {
@@ -80,10 +93,7 @@ short ALawDecompressTable[256] =
       944,   912,  1008,   976,   816,   784,   880,   848
 };
 
-//======== COMPRESS FUNCTIONS =================================
-
-const int cBias = 0x84;
-const int cClip = 32635;
+//======== COMPRESS FUNCTIONS ==================================================
 
 static char MuLawCompressTable2[256] =
 {
@@ -129,141 +139,178 @@ static char ALawCompressTable2[128] =
 unsigned char MuLawCompressTable[16384];
 unsigned char ALawCompressTable[16384];
 
-
-//------------------------------------------------------------
+//------------------------------------------------------------------------------
 unsigned char MuLawALawRecompressTable[256];
 unsigned char ALawMuLawRecompressTable[256];
 
-#if 0 //not-dahdi version
-//------------------------------------------------------------
-inline uint8_t  linear2mulaw2(int16_t sample)
+
+#if !defined(top_bit)
+//------------------------------------------------------------------------------
+int top_bit(unsigned int bits)
 {
-     int sign = (sample >> 8) & 0x80;
-     if (sign)
-          sample = (short)-sample;
-     if (sample > cClip)
-          sample = cClip;
-     sample = (short)(sample + cBias);
-     int exponent = (int)MuLawCompressTable2[(sample>>7) & 0xFF];
-     int mantissa = (sample >> (exponent+3)) & 0x0F;
-     int compressedByte = ~ (sign | (exponent << 4) | mantissa);
+    int res;
 
-     return (unsigned char)compressedByte;
+    if (bits == 0)
+        return -1;
+    res = 0;
+    if (bits & 0xFFFF0000)
+    {
+        bits &= 0xFFFF0000;
+        res += 16;
+    }
+    if (bits & 0xFF00FF00)
+    {
+        bits &= 0xFF00FF00;
+        res += 8;
+    }
+    if (bits & 0xF0F0F0F0)
+    {
+        bits &= 0xF0F0F0F0;
+        res += 4;
+    }
+    if (bits & 0xCCCCCCCC)
+    {
+        bits &= 0xCCCCCCCC;
+        res += 2;
+    }
+    if (bits & 0xAAAAAAAA)
+    {
+        bits &= 0xAAAAAAAA;
+        res += 1;
+    }
+    return res;
 }
-#else //dahdi version
-
-#define ZEROTRAP    /* turn on the trap as per the MIL-STD */
-#define BIAS 0x84   /* define the add-in bias for 16 bit samples */
-#define CLIP 32635
-
-inline uint8_t  linear2mulaw2(int16_t sample)
-{
-  static int exp_lut[256] = {0,0,1,1,2,2,2,2,3,3,3,3,3,3,3,3,
-                             4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
-                             5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
-                             5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
-                             6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-                             6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-                             6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-                             6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-                             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7};
-  int sign, exponent, mantissa;
-  unsigned char ulawbyte;
-
-  /* Get the sample into sign-magnitude. */
-  sign = (sample >> 8) & 0x80;          /* set aside the sign */
-  if (sign != 0) sample = -sample;      /* get magnitude */
-  if (sample > CLIP) sample = CLIP;     /* clip the magnitude */
-
-  /* Convert from 16 bit linear to ulaw. */
-  sample = sample + BIAS;
-  exponent = exp_lut[(sample >> 7) & 0xFF];
-  mantissa = (sample >> (exponent + 3)) & 0x0F;
-  ulawbyte = ~(sign | (exponent << 4) | mantissa);
-#ifdef ZEROTRAP
-  if (ulawbyte == 0) ulawbyte = 0x02;      /* optional CCITT trap */
-#endif
-  if (ulawbyte == 0xff) ulawbyte = 0x7f;   /* never return 0xff */
-  return(ulawbyte);
-}
-
 #endif
 
+/*
+ * Mu-law is basically as follows:
+ *
+ *      Biased Linear Input Code        Compressed Code
+ *      ------------------------        ---------------
+ *      00000001wxyza                   000wxyz
+ *      0000001wxyzab                   001wxyz
+ *      000001wxyzabc                   010wxyz
+ *      00001wxyzabcd                   011wxyz
+ *      0001wxyzabcde                   100wxyz
+ *      001wxyzabcdef                   101wxyz
+ *      01wxyzabcdefg                   110wxyz
+ *      1wxyzabcdefgh                   111wxyz
+ *
+ * Each biased linear code has a leading 1 which identifies the segment
+ * number. The value of the segment number is equal to 7 minus the number
+ * of leading 0's. The quantization interval is directly available as the
+ * four bits wxyz.  * The trailing bits (a - h) are ignored.
+ *
+ * Ordinarily the complement of the resulting code word is used for
+ * transmission, and so the code word is complemented before it is returned.
+ *
+ * For further information see John C. Bellamy's Digital Telephony, 1982,
+ * John Wiley & Sons, pps 98-111 and 472-476.
+ */
 
-#if 0 //not-dahdi version
-//------------------------------------------------------------
-inline uint8_t  linear2alaw2(int16_t sample)
+/* Enable the trap as per the MIL-STD */
+/* #define G711_ULAW_ZEROTRAP */
+
+/* Bias for u-law encoding from linear. */
+#define G711_ULAW_BIAS      0x84
+
+//------------------------------------------------------------------------------
+/* Encode a linear sample to u-law */
+inline uint8_t  linear2mulaw2(int16_t sample)
 {
-     int sign;
-     int exponent;
-     int mantissa;
-     unsigned char compressedByte;
+    uint8_t u_val;
+    int mask;
+    int seg;
 
-     sign = ((~sample) >> 8) & 0x80;
-     if (!sign)
-          sample = (short)-sample;
-     if (sample > cClip)
-          sample = cClip;
-     if (sample >= 256)
-     {
-          exponent = (int)ALawCompressTable2[(sample >> 8) & 0x7F];
-          mantissa = (sample >> (exponent + 3) ) & 0x0F;
-          compressedByte = ((exponent << 4) | mantissa);
-     }
-     else
-     {
-          compressedByte = (unsigned char)(sample >> 4);
-     }
-     compressedByte ^= (sign ^ 0x55);
-     return compressedByte;
-} 
-#else //dahdi version
+    /* Get the sign and the magnitude of the value. */
+    if (sample >= 0)
+    {
+        sample = G711_ULAW_BIAS + sample;
+        mask = 0xFF;
+    }
+    else
+    {
+        sample = G711_ULAW_BIAS - sample;
+        mask = 0x7F;
+    }
 
-#define AMI_MASK 0x55
+    seg = top_bit(sample | 0xFF) - 7;
+
+    /*
+     * Combine the sign, segment, quantization bits,
+     * and complement the code word.
+     */
+    if (seg >= 8)
+        u_val = (uint8_t) (0x7F ^ mask);
+    else
+        u_val = (uint8_t) (((seg << 4) | ((sample >> (seg + 3)) & 0xF)) ^ mask);
+#if defined(G711_ULAW_ZEROTRAP)
+    /* Optional ITU trap */
+    if (u_val == 0)
+        u_val = 0x02;
+#endif
+    return  u_val;
+}
+
+/*
+ * A-law is basically as follows:
+ *
+ *      Linear Input Code        Compressed Code
+ *      -----------------        ---------------
+ *      0000000wxyza             000wxyz
+ *      0000001wxyza             001wxyz
+ *      000001wxyzab             010wxyz
+ *      00001wxyzabc             011wxyz
+ *      0001wxyzabcd             100wxyz
+ *      001wxyzabcde             101wxyz
+ *      01wxyzabcdef             110wxyz
+ *      1wxyzabcdefg             111wxyz
+ *
+ * For further information see John C. Bellamy's Digital Telephony, 1982,
+ * John Wiley & Sons, pps 98-111 and 472-476.
+ */
+
+/* The A-law alternate mark inversion mask */
+#define G711_ALAW_AMI_MASK      0x55
+
+//------------------------------------------------------------------------------
+/* Encode a linear sample to A-law */
 inline uint8_t  linear2alaw2(int16_t sample)
 {
     int mask;
     int seg;
-    int pcm_val;
-    static int seg_end[8] =
+    
+    if (sample >= 0)
     {
-         0xFF, 0x1FF, 0x3FF, 0x7FF, 0xFFF, 0x1FFF, 0x3FFF, 0x7FFF
-    };
-
-    pcm_val = sample;
-    if (pcm_val >= 0)
-    {
-        /* Sign (7th) bit = 1 */
-        mask = AMI_MASK | 0x80;
+        /* Sign (bit 7) bit = 1 */
+        mask = G711_ALAW_AMI_MASK | 0x80;
     }
     else
     {
-        /* Sign bit = 0 */
-        mask = AMI_MASK;
-        pcm_val = -pcm_val;
+        /* Sign (bit 7) bit = 0 */
+        mask = G711_ALAW_AMI_MASK;
+        sample = -sample - 1;
     }
 
     /* Convert the scaled magnitude to segment number. */
-    for (seg = 0;  seg < 8;  seg++)
+    seg = top_bit(sample | 0xFF) - 7;
+    if (seg >= 8)
     {
-        if (pcm_val <= seg_end[seg])
-	    break;
+        if (sample >= 0)
+        {
+            /* Out of range. Return maximum value. */
+            return (uint8_t) (0x7F ^ mask);
+        }
+        /* We must be just a tiny step below zero */
+        return (uint8_t) (0x00 ^ mask);
     }
     /* Combine the sign, segment, and quantization bits. */
-    return  ((seg << 4) | ((pcm_val >> ((seg)  ?  (seg + 3)  :  4)) & 0x0F)) ^ mask;
+    return (uint8_t) (((seg << 4) | ((sample >> ((seg)  ?  (seg + 3)  :  4)) & 0x0F)) ^ mask);
 }
-#endif
 
 
 static char needinitg711 = 1;
-//------------------------------------------------------------
+//------------------------------------------------------------------------------
 inline void init_g711(void)
 {
     int           i;
