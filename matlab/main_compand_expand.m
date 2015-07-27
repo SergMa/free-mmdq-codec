45c3958b55c90ce19394a56b2e7a2daa3adc7252
Serg Mashkin <mashkh@yandex.ru>
2015-07-27 17:20:01 +0600

    MATLAB: show plots of quantized compand/expand

--- matlab/main_compand_expand.m
+++ matlab/main_compand_expand.m
@@ -46,3 +46,79 @@ legend('ver0','ver1','ver2','ver3');
 xlabel('x');
 ylabel('y');
 grid on;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Quantized versions for defined factor
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+AMPDV = 2*32768;
+BITS_PER_SAMPLE = 3;
+FACTOR = 2^BITS_PER_SAMPLE;
+
+%Fill compand table
+x  = -AMPDV:1:AMPDV;
+N = length(x);
+y  = zeros(4,N);
+for s=1:4
+    for i=1:N
+        dvoice = x(i);
+        sss = compand( dvoice/AMPDV , s-1 );
+        ddd = FACTOR/2 * sss + FACTOR/2;
+        ddd = fix(ddd);
+        ddd = min(ddd,FACTOR-1);
+        y(s,i) = fix( ddd );
+    end
+end
+
+%Fill expand table
+v = 0:FACTOR-1;
+z = zeros(4,FACTOR);
+for s=1:4
+    for i=1:FACTOR
+        dvoice = i-1;
+        sss = expand( (dvoice+0.5)/FACTOR - 0.5 , s-1 );
+        z(s,i) = sss;
+    end
+end
+
+%expand(compand(inp))
+y2 = zeros(4,N);
+for s=1:4
+    for i=1:N
+        inp = x(i);
+        com = y(s,inp+AMPDV+1);
+        exp = 2*AMPDV*z(s,com+1);
+        y2(s,i) = exp;
+    end
+end
+
+%Plot graphics
+figure(4);
+for s=1:4
+    subplot(2,2,s);
+    plot(x,y(s,:),'r.-');
+    title(['compand ver.',num2str(s)]);
+    xlabel('inp');
+    ylabel('out');
+    grid on;
+end
+
+figure(5);
+for s=1:4
+    subplot(2,2,s);
+    plot(v,z(s,:),'r.-');
+    title(['expand ver.',num2str(s)]);
+    xlabel('inp');
+    ylabel('out');
+    grid on;
+end
+
+figure(6);
+for s=1:4
+    subplot(2,2,s);
+    plot(x,y2(s,:),'r.-', x,x,'b');
+    title(['expand(compand(inp)) ver.',num2str(s)]);
+    xlabel('inp');
+    ylabel('out');
+    grid on;
+end
