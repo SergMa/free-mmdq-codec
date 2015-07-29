f735b5ff9982bded9579de0b55bcaa0cd659c3a2
Serg Mashkin <mashkh@yandex.ru>
2015-07-29 15:04:55 +0600

    MATLAB: decoder.m - cosmetics

--- matlab/decoder.m
+++ matlab/decoder.m
@@ -34,35 +34,32 @@ function [voice,dec] = decoder(data,dec)
     maxdv = max(dvoice);
     diffdv = maxdv - mindv;
 
-    if 1
-        if smooth0==0 && smooth1==0
-            voice(1) = 0;
-            for i=1:N-1
-                sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 0 );
-                voice(i+1) = voice(i) + sss;
-                %fprintf(1,'voice(i=%3d)=%8.3f , sss=%8.3f, voice(i+1)=%8.3f\n', i, voice(i), sss, voice(i+1));
-            end
-        elseif smooth0==1 && smooth1==0
-            %expand/compand smoothing
-            voice(1) = 0;
-            for i=1:N-1
-                sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 1 );
-                voice(i+1) = voice(i) + sss;
-            end
-        elseif smooth0==0 && smooth1==1
-            %expand/compand smoothing
-            voice(1) = 0;
-            for i=1:N-1
-                sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 2 );
-                voice(i+1) = voice(i) + sss;
-            end
-        else
-            %expand/compand smoothing
-            voice(1) = 0;
-            for i=1:N-1
-                sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 3 );
-                voice(i+1) = voice(i) + sss;
-            end
+    if smooth0==0 && smooth1==0
+        voice(1) = 0;
+        for i=1:N-1
+            sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 0 );
+            voice(i+1) = voice(i) + sss;
+        end
+    elseif smooth0==1 && smooth1==0
+        %expand/compand smoothing
+        voice(1) = 0;
+        for i=1:N-1
+            sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 1 );
+            voice(i+1) = voice(i) + sss;
+        end
+    elseif smooth0==0 && smooth1==1
+        %expand/compand smoothing
+        voice(1) = 0;
+        for i=1:N-1
+            sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 2 );
+            voice(i+1) = voice(i) + sss;
+        end
+    else
+        %expand/compand smoothing
+        voice(1) = 0;
+        for i=1:N-1
+            sss = expand( ((dvoice(i)+0.5)/dec.factor - 0.5)*2 , 3 );
+            voice(i+1) = voice(i) + sss;
         end
     end
 
