d35ae7a274a94223fa0f2f321bd2965f5a20aba8
Serg Mashkin <mashkh@yandex.ru>
2018-03-13 13:04:03 +0600

    main_compand_expand.m: change s=0..3 to s=1..4

--- matlab/main_compand_expand.m
+++ matlab/main_compand_expand.m
@@ -6,46 +6,55 @@ clc;
 clear all;
 close all;
 
-x=-1:0.001:1;
+global COM_PWR;
+global EXP_PWR;
 
-c0 = compand(x,0);
+%compand/expand functions powers
+COM_PWR = [1.0  1.10  1.20  1.20];  % best for MMDQ-32
+EXP_PWR = [1.0  1.10  1.20  1.20];
+
+x=-1:0.05:1;
+
 c1 = compand(x,1);
 c2 = compand(x,2);
 c3 = compand(x,3);
+c4 = compand(x,4);
 
-e0 = expand(x,0);
 e1 = expand(x,1);
 e2 = expand(x,2);
 e3 = expand(x,3);
+e4 = expand(x,4);
 
-z0 = expand(compand(x,0),0);
 z1 = expand(compand(x,1),1);
 z2 = expand(compand(x,2),2);
 z3 = expand(compand(x,3),3);
+z4 = expand(compand(x,4),4);
 
 figure(1);
-plot(x,c0,'r.-', x,c1,'g.-', x,c2,'b.-', x,c3,'k.-');
+plot(x,c1,'k-', x,c2,'k-x', x,c3,'k.-', x,c4,'k-+');
 title('compand(x)');
-legend('ver0','ver1','ver2','ver3');
+legend('ver1','ver2','ver3','ver4');
 xlabel('x');
 ylabel('y');
 grid on;
 
 figure(2);
-plot(x,e0,'r.-', x,e1,'g.-', x,e2,'b.-', x,e3,'k.-');
+plot(x,e1,'k-', x,e2,'k-x', x,e3,'k.-', x,e4,'k-+');
 title('expand(x)');
-legend('ver0','ver1','ver2','ver3');
+legend('ver1','ver2','ver3','ver4');
 xlabel('x');
 ylabel('y');
 grid on;
 
 figure(3);
-plot(x,z0,'r.-', x,z1,'g.-', x,z2,'b.-', x,z3,'k.-');
+plot(x,z1,'k-', x,z2,'k-x', x,z3,'k.-', x,z4,'k-+');
 title('expand(compand(x))');
-legend('ver0','ver1','ver2','ver3');
+legend('ver1','ver2','ver3','ver4');
 xlabel('x');
 ylabel('y');
 grid on;
+
+break
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Quantized versions for defined factor
@@ -62,7 +71,7 @@ y  = zeros(4,N);
 for s=1:4
     for i=1:N
         dvoice = x(i);
-        sss = compand( dvoice/AMPDV , s-1 );
+        sss = compand( dvoice/AMPDV , s );
         ddd = FACTOR/2 * sss + FACTOR/2;
         ddd = fix(ddd);
         ddd = min(ddd,FACTOR-1);
@@ -76,7 +85,7 @@ z = zeros(4,FACTOR);
 for s=1:4
     for i=1:FACTOR
         dvoice = i-1;
-        sss = expand( (dvoice+0.5)/FACTOR - 0.5 , s-1 );
+        sss = expand( (dvoice+0.5)/FACTOR - 0.5 , s );
         z(s,i) = sss;
     end
 end
